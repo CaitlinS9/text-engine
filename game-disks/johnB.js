e11eb3351a7f36569316d64a8792e90407416bc2
Caitlin Stewart <CaitlinCalhoun@gmail.com>
2017-04-09 21:48:55 -0500

    alpha, ready to fully playtest
    still need more ascii images, fact checking/typos, & file formatting

--- game-disks/johnB.js
+++ game-disks/johnB.js
@@ -29,9 +29,6 @@ const johnB = {
         Located in Bibb County, Shittown (officially known as Woodstock) is a small, sorry ass town with a population of about 1,500. Looking around the town you can see several churches but not a damn bookstore. Poverty abounds and with poverty comes corruption.
         You find yourself on the property of John B. McLemore.
       `,
-      items: [
-        { name: 'thing', desc: 'It\'s a thing!.' },
-      ],
       exits: [
         { dir: 'north', id: 'house' }
       ]
@@ -56,40 +53,11 @@ const johnB = {
       desc: `
         You come upon a house. It doesn't look like much but it could be described as stately. Aside from the sound of barking dogs you don't hear much - there is no loud hum of air conditioning. You realize that the sound of the dogs might just be a memory. Looking around, you don't see anything that would indicate that the owner of this house has any posessions of value.
       `,
-      items: [
-        { name: 'key', desc: 'It looks like a key.', isTakeable: true, use: ({disk, println, getRoom}) => {
-          const room = getRoom(disk.roomId);
-          const door = room.items.find(item => item.name === 'door');
-          if (door) {
-            println('The door has opened!');
-            door.isOpen = true;
-          } else {
-            println('There\'s nothing to use the key on.');
-          }
-        }},
-        { name: 'book', desc: 'It appears to contain some sort of encantation, or perhaps... code.', isTakeable: true, use: ({disk, println, getRoom}) => {
-          const room = getRoom(disk.roomId);
-          const door = room.items.find(item => item.name === 'door');
-
-          if (door) {
-            println('You already used the book!');
-            return;
-          }
-
-          println('A door has appeared from nothing! It seems to go nowhere...');
-          room.items.push({ name: 'door', desc: 'It seems to go nowhere...', isOpen: false, use: ({disk, println, enterRoom}) => {
-            const door = room.items.find(item => item.name === 'door');
-            if (door.isOpen) {
-              enterRoom('gameReallyOver');
-            } else {
-              println('The door is locked.');
-            }
-          }});
-        }},
-        { name: 'castle', desc: 'It has been... corrupted somehow.' },
-      ],
       exits: [
-        { dir: 'north', id: 'path' }
+        { dir: 'north', id: 'porch' },
+          { dir: 'south', id: 'shittown' },
+          { dir: 'west', id: 'clearedLand' },
+          { dir: 'east', id: 'forest2' }
       ]
     },
  {
@@ -100,11 +68,11 @@ const johnB = {
       desc: `
         You find yourself on a path. Ahead, you see a grey stone wall, lovingly crafted. There are round pilars at either end of the wall and the wall increases in height with a slight curve.
       `,
-      items: [
-        { name: 'thing', desc: 'It\'s a thing!.' },
-      ],
       exits: [
-        { dir: 'north', id: 'maze' }
+        { dir: 'west', id: 'maze' },
+          { dir: 'north', id: 'forest' },
+          { dir: 'south', id: 'garden' },
+          { dir: 'east', id: 'land' }
       ]
     },
       {
@@ -118,7 +86,7 @@ const johnB = {
       items: [
           { name: 'watering can', desc: 'It\'s a watering can full of water.', isTakeable: true, use: ({disk, println, getRoom}) => {
           const room = getRoom(disk.roomId);
-          if (room == 'maze') {
+          if (room == 'maze' || room == 'garden') {
             println('Your pour a bit on water on the rosebushes. It seems to make you feel a bit better although there is still a lot of work to be done.');
           } else {
             println('That seems wasteful. Is this the best place to use this?');
@@ -126,7 +94,10 @@ const johnB = {
         }},
       ],
       exits: [
-        { dir: 'north', id: 'maze' }
+        { dir: 'north', id: 'center' },
+          { dir: 'south', id: 'forest' },
+          { dir: 'east', id: 'path' },
+          { dir: 'west', id: 'forest' }
       ]
     },
       {
@@ -145,7 +116,8 @@ const johnB = {
           } else {
             println('That seems wasteful. Is this the best place to use this?');
           }
-        }},
+        }
+      },
           {
               name: 'table', desc: 'On the table there is an assortment of tools as well as alcohol. You see a few droplets of mercury on the table... Amongst the tools you notice a piercing gun as well as a bottle of liquid.'
           },
@@ -157,14 +129,19 @@ const johnB = {
                       {
                          if (inInventory('telephone'))
                              {
-                                 //TODO: Check if the phone was *just* used. last command?
-                                 //if yes
-                                 //TODO: end game text and room
-                                 enterRoom('endGame');
+                                 //Check if the phone was *just* used. 
+                                 if (disk.input[input.length - 1] == 'use telephone')
+                                 {
+                                     enterRoom('endGame');
+                                 }
+                                 else
+                                 {
+                                      println('You don\'t want to do this alone. There must be someone that you can call to make sure that your wishes are observed.');
+                                 }
                              }
                           else
                               {
-                                  println('You don\'t want to do this alone. There must be someone that you can call to make sure that your wishes are observer.');
+                                  println('You don\'t want to do this alone. There must be someone that you can call to make sure that your wishes are observed.');
                               }
                       }
                   else
@@ -186,14 +163,16 @@ const johnB = {
               }
           },
           {
-            name: 'shovel', desc: 'This is a normal shovel used to dig holes.', isTakeable: true, use: (disk, println)
+            name: 'shovel', desc: 'This is a normal shovel used to dig holes.', isTakeable: true, use: ({disk, println}) =>
               {
               println('You dig the shovel into the earth. You dig deeper and deeper. John B. say he was unbanked and worth millions. He definitely lived comfortably, always talking about gold. It\'s got to be burried here somewhere. This has got to be the right spot. Unfortunately, you find nothing... but you know it is here somewhere. It\'s got to be');
           }
           },
       ],
       exits: [
-        { dir: 'north', id: 'maze' }
+        { dir: 'north', id: 'slab' },
+          { dir: 'south', id: 'forest' },
+          { dir: 'east', id: 'forest' }
       ]
     },
      {
@@ -202,7 +181,7 @@ const johnB = {
       img: `                    
       `,
       desc: `
-        You enter the kitchen of John B.\'s house. A window above the sink overlooks _____*****______. There is a telephone on the wall and a bottle of Tums sits on the counter. In the corner, there is a potbelly stove. As with most kitchens, there is a fridge.
+        You enter the kitchen of John B.\'s house. A window above the sink lets in light. There is a telephone on the wall and a bottle of Tums sits on the counter. In the corner, there is a potbelly stove. As with most kitchens, there is a fridge.
       `,
       items: [
           { name: 'fridge', desc: 'This fridge is nothing special, it looks like any fridge.', use: ({disk, println}) => {
@@ -218,7 +197,7 @@ const johnB = {
                 }
           },
           {
-          name: 'telephone', desc: 'This looks like a cordless landline phone.', isTakeable: true, use ({disk, println, getRoom}) =>
+          name: 'telephone', desc: 'This looks like a cordless landline phone.', isTakeable: true, use: ({disk, println, getRoom}) =>
               {
                   const room = getRoom(disk.roomId);
 
@@ -237,7 +216,7 @@ const johnB = {
           },
       ],
       exits: [
-        { dir: 'north', id: 'maze' }
+        { dir: 'south', id: 'houseInterior' }
       ]
     },
        {
@@ -262,7 +241,7 @@ const johnB = {
           }
       ],
       exits: [
-        { dir: 'north', id: 'maze' }
+        { dir: 'east', id: 'houseInterior' }
       ]
     },
       {
@@ -274,10 +253,10 @@ const johnB = {
         You continue until you reach the center of the maze. What an accomplishment! With so many potential solutions, you can\'t help but admire the craftsmanship, even if the maze hasn\'t been kept up. This place feels special. You know that this place is important. It\'d be a great place to keep something special or important.
       `,
       exits: [
-        { dir: 'north', id: 'maze' },
+        { dir: 'north', id: 'forest' },
           {dir: 'south', id: 'maze'},
-          {dir: 'east', id: 'maze'},
-          {dir: 'west', id: 'maze'}
+          {dir: 'east', id: 'forest'},
+          {dir: 'west', id: 'forest'}
       ]
     },
       {
@@ -289,10 +268,10 @@ const johnB = {
        You see that the ground is more solid now. There is a slab of cement. Looking closely you can see discoloration where it appears that vehicles were stored. One place is definitely large enough to have been a bus or large trailer.
       `,
       exits: [
-        { dir: 'north', id: 'maze' },
-          {dir: 'south', id: 'maze'},
-          {dir: 'east', id: 'maze'},
-          {dir: 'west', id: 'maze'}
+        { dir: 'north', id: 'forest' },
+          {dir: 'south', id: 'shop'},
+          {dir: 'east', id: 'land'},
+          {dir: 'west', id: 'forest'}
       ]
     },
       {
@@ -315,10 +294,104 @@ const johnB = {
         }
     ],
       exits: [
-        {dir: 'north', id: 'maze'},
-          {dir: 'south', id: 'maze'},
-          {dir: 'east', id: 'maze'},
-          {dir: 'west', id: 'maze'}
+        {dir: 'north', id: 'houseInterior'},
+          {dir: 'south', id: 'house'}
+      ]
+    },
+      {
+      name: 'The Land',
+      id: 'land',
+      img: `                    
+      `,
+      desc: `
+      You find yourself on a nice piece of land. You can see that whoever owns this land really cares for it. Outside of the clearing, you are surrounded by trees.
+      `,
+      exits: [
+        {dir: 'north', id: 'forest'},
+          {dir: 'east', id: 'slab'},
+          {dir: 'west', id: 'path'}
+      ]
+    },
+           {
+      name: 'The Forest',
+      id: 'forest',
+      img: `                    
+      `,
+      desc: `
+      You are surrounded by trees. You get the feeling that there's over 100 acres of trees around. There doesn't seem to be much of anything else around.
+      `,
+      exits: [
+        {dir: 'north', id: 'land'},
+          {dir: 'south', id: 'land'},
+          {dir: 'east', id: 'land'},
+          {dir: 'west', id: 'land'}
+      ]
+    },
+   {
+      name: 'The Forest',
+      id: 'forest2',
+      img: `                    
+      `,
+      desc: `
+      You are surrounded by trees. You get the feeling that there's over 100 acres of trees around. There doesn't seem to be much of anything else around.
+      `,
+      exits: [
+          {dir: 'east', id: 'house'},
+          {dir: 'west', id: 'house'}
+      ]
+    },
+    {
+      name: 'The Garden',
+      id: 'garden',
+      img: `                    
+      `,
+      desc: `
+      You come across a garden. There's quite a variety of plants here, some of which you have never heard of. There is a large leafy plant that you recognise as Aspidistra elatior.
+      `,
+      exits: [
+          {dir: 'north', id: 'path'},
+          {dir: 'west', id: 'forest'},
+          { dir: 'south', id: 'clearedLand' }
+      ]
+    },
+    {
+      name: 'Cleared Land',
+      id: 'clearedLand',
+      img: `                    
+      `,
+      desc: `
+      You find yourself on a bit of land that has been cleared. From here you can see some land that looks like it was maintained at some point. The ground is dusty and there is some debris.
+      `,
+        items: [
+            {
+                name: 'ground', desc: 'The ground is dusty but among the debris you can make out a gear'
+            },
+            {
+                name: 'debris', desc: 'The ground is dusty but among the debris you can make out a gear'
+            },
+            {
+                name: 'gear', desc: 'The gear is older and appears to be gold. It looks like it is the appropriate size for a clock.', isTakeable: true
+            }
+        ],
+      exits: [
+          {dir: 'north', id: 'garden'},
+          {dir: 'south', id: 'forest2'},
+          {dir: 'west', id: 'forest'},
+          { dir: 'east', id: 'house' }
+      ]
+    },
+    {
+      name: 'Inside the House',
+      id: 'houseInterior',
+      img: `                    
+      `,
+      desc: `
+      You enter the house. It doesn't look like much.
+      `,
+      exits: [
+        {dir: 'north', id: 'kitchen'},
+          {dir: 'south', id: 'porch'},
+          {dir: 'west', id: 'office'}
       ]
     },
       {
@@ -327,7 +400,13 @@ const johnB = {
       img: `                    
       `,
       desc: `
-      ***FIX ME!!!**** Something horrible about death and suicide and the assumed reasons why. The pain of the cyanide, what he told faye about it. etc etc
+        'Usually when you see jokers that look like that they done something to get like that.'
+
+        Maybe it was all the mercury that caused you to feel like this was the answer, maybe it was the realization that at age 50 all you have to look forward to in life is decline - all of your good moments have been spent, maybe it is because the world is a terrible place and can't survive the abuse it is receiving, maybe you're just lonely and feel like no one truely cares about you. It doesn't matter. Whatever the reason, you pick up the bottle of cyanide and begin to drink it.
+
+        You make sure to tell Faye who to contact in the event of your death and let her know that if she calls the cops that you'll shoot them once you see them. She'll want to keep them safe... even then you know that the cyanide will have done its job well before the police would ever make it out to you. 
+
+        But the cyanide is more painful that you expected. You feel it burn as it goes down your throat. You scream in pain and anguish and writhe on the ground. You struggle ot breathe and no matter what you do you just can't seem to get any air. In a matter of minutes you're dead. 
       `,
     },
   ],
